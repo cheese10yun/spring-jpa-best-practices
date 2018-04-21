24e947c64adf1c2adea2c5a5c11dbfa289018157
Yun <cheese10yun@gmail.com>
2018-04-21 23:16:01 +0900

    First Commit

---
+++ src/test/java/com/cheese/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cheese.springjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
