64db92f8b62fa49ff07f63e97d470c1a04ff4b9c
Yun <cheese10yun@gmail.com>
2018-04-28 03:54:01 +0900

    Step-02 작업중

---
+++ doc/step-02.md
@@ -0,0 +1,3 @@
+# Step-02
+
+Spring Boot `@RestControllerAdvice`를 이용한 예외 처리  
--- src/main/java/com/cheese/springjpa/Account/AccountRepository.java
+++ src/main/java/com/cheese/springjpa/Account/AccountRepository.java
@@ -3,4 +3,6 @@ package com.cheese.springjpa.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Account findByEmail(String email);
 }
