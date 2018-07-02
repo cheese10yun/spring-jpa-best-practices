01234f722460f7dbb1fdf4a28ba32f84c83d8e49
Yun <cheese10yun@gmail.com>
2018-07-02 23:41:22 +0900

    test

--- src/test/java/com/cheese/springjpa/account/model/EmailTest.java
+++ src/test/java/com/cheese/springjpa/account/model/EmailTest.java
@@ -9,7 +9,6 @@ public class EmailTest {
 
     @Test
     public void email() {
-
         final String id = "test";
         final String host = "@test.com";
         final Email email = Email.builder()
