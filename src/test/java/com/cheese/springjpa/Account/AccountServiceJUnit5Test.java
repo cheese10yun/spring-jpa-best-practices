4a3163e20ec0ad959b802b331c930c20835089ba
Yun <cheese10yun@gmail.com>
2019-02-06 21:01:55 +0900

    step 14 추가

--- src/test/java/com/cheese/springjpa/Account/AccountServiceJUnit5Test.java
+++ src/test/java/com/cheese/springjpa/Account/AccountServiceJUnit5Test.java
@@ -5,8 +5,6 @@ import com.cheese.springjpa.Account.model.Email;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,7 +20,6 @@ import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 public class AccountServiceJUnit5Test {
 
 
